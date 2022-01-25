8e35a994c0e2ec7cdaadab8d56e2a7f1946f8995
tarun9142 <tarunkumar9142@gmail.com>
2022-01-25 19:02:29 +0530

    First commit: create folder structure and setup express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express  = require('express');
+const app = express();
+port = 8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log('error running the server');
+    }
+    console.log(`server is up and running at port ${port}`);
+})
