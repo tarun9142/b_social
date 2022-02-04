c4582d8397e27e2390d7281507122cab781786b8
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-02-04 21:35:07 +0530

    setup an create layout for app

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express  = require('express');
 const app = express();
 port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+
+// use express router 
 app.use('/',require('./routes'));
+
+// set up the view engine 
 app.set('view engine','ejs');
 app.set('views','./views');
 
