cef222569ffd882c1286264d50b728abf02a0fa3
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-02-16 21:20:19 +0530

    restrict unauthorized user from posting and hide the post form if the user is unauthorized

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,12 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    return res.render('home',
-    {title: 'home'}
-    );
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',
+        {
+            title: 'b_social | home',
+            posts: posts
+        }
+        );
+    })
 }
