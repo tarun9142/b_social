148eeab0accf63368a9191a1094c62ed025e89d7
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-02-17 11:04:53 +0530

    create form for posting comment and resolve a typo in post.js

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({
     },
     comments:[
         {
-            type: mongoose.Schema.Types,ObjectId,
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
     ]
