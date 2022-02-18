447b45f4782ddc269f54949f6ea847ddfe6511d5
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-02-18 20:50:11 +0530

    add stylings for homepage and improve some previous stylings

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,7 +13,7 @@ module.exports.update = function(req,res){
     if(req.user.id == req.params.id){
         User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
             return res.redirect('back');
-        })
+        });
     }else{
         return res.status(401).send('unauthorized');
     }
