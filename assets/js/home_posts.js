a674b2a9c1e9b2276d82691779c4814af3b45f82
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-03-08 19:16:12 +0530

    delete dynamically added post using ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,19 +1,17 @@
 {
-    // method to create a post using AJAX 
+    // method to submit form data for new post using AJAX 
     let createPost = function(){
         let newPostForm = $('#new-post-form');
-
         newPostForm.submit(function(e){
             e.preventDefault();
-
             $.ajax({
                 type: 'post',
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
-                    let newPost = newPostDom(data.data.post);
-                    $('#posts-container>ul').prepend(newPost);
+                    let newPost = newPostDom(data.data.post,data.data.user_name);
+                    $("#posts-container>ul").prepend(newPost);
+                    deletePost($(newPost).find('.delete-post-button'));
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -21,23 +19,21 @@
         });
     }
 
-    // method to create post in DOM 
+    // method to create a post in DOM 
 
-    let newPostDom = function(post){
-        return $(`<li id="post${ post._id}">
+    let newPostDom = function(post,user_name){
+        return $(`<li id="post-${ post._id}">
                     <p>
-                
                     <small>
                         <a
-                        class="post-delete-button"
-                        href="/posts/destroy/${post.id}"
+                        class="delete-post-button"
+                        href="/posts/destroy/${post._id}"
                         style="color: black"
-                        >X</a
-                        >
+                        >X</a>
                     </small>
                     ${post.content}
                     <br />
-                    <small> ${post.user.name}</small>
+                    <small> ${user_name}</small>
                     </p>
                     
                     <div class="post-comment">
@@ -58,6 +54,24 @@
                 </li>
       `);
     };
+
+
+    //method to delete a post from DOM
+
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(event){
+            event.preventDefault();
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success:function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(error){
+                    console.log(error.responseText);
+                }
+            });
+        });
+    }
 
 
     createPost();
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const User = require('../models/user');
 
 module.exports.create = async function(req,res){
     try {
@@ -7,16 +8,17 @@ module.exports.create = async function(req,res){
             content: req.body.content,
             user: req.user._id
         });
-
+        let user = await User.findById(post.user);
         if(req.xhr){
             return res.status(200).json({
                 data: {
-                    post: post
+                    post: post,
+                    user_name: user.name
                 },
                 message: "post created!"
             });
         }
-        req.flash('success','successfully posted');
+        req.flash('success','posted successfully');
         return res.redirect('back');
     } catch (err) {
         req.flash('error','error posting');
@@ -31,8 +33,17 @@ module.exports.destroy = async function(req,res){
         if(post.user == req.user.id){
             post.remove();
             await Comment.deleteMany({post: req.params.id});
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id: req.params.id
+                    },
+                    message:'post deleted'
+                });
+            }
+
             req.flash('success','successfully deleted post and related comments');
-            return res.redirect('back');
+           return res.redirect('back');
         }else{
             req.flash('error','you cannot delete this post');
             return res.redirect('back');
