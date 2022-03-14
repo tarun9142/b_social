a2b7d0f0478415d9f4ae4446dcc6d6555123f0dc
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-03-14 11:16:46 +0530

    setup api and  render posts,detete a post using api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const homeController = require('../controllers/home_controller');
 
 console.log('router working');
@@ -8,5 +9,7 @@ router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
+
+router.use('/api',require('./api'));
 
 module.exports = router;
