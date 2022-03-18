b33a3f12a39a6e11643f97f2f120fd9bffacdc1b
Tarun kumar <79507487+tarun9142@users.noreply.github.com>
2022-03-18 16:20:03 +0530

    send html template emails

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,11 +1,14 @@
 const nodeMailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
+
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs'); 
+
     nodeMailer.transporter.sendMail({
         from: 'tarun kumar',
         to:comment.user.email,
         subject: 'new comment published',
-        html: '<h1>new comment posted</h1>'
+        html: htmlString
     },(err,info) => {
         if(err){
             console.log('error in sending mail',err);
